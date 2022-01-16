4e016c5800624dbe56243f97bff89d613e3d46c9
sachintha00 <sachintha0603@gmail.com>
2022-01-16 18:13:24 +0530

    Create the library for the getting details by nic

---
+++ NIC Details Finder/src/main/java/com/smd/nicdetailsfinder/NICDetailFinder.java
@@ -0,0 +1,10 @@
+package com.smd.nicdetailsfinder;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class NICDetailFinder {
+    public static void testLibrary(Context context,String msg){
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+}
