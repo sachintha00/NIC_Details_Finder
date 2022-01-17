62f51cbee781dc4ab98a7e424f25cf9b529281ad
sachintha00 <sachintha0603@gmail.com>
2022-01-17 09:52:58 +0530

    updated

--- app/src/main/java/com/smd/nicdetailsfinder/MainActivity.java
+++ app/src/main/java/com/smd/nicdetailsfinder/MainActivity.java
@@ -3,12 +3,21 @@ package com.smd.nicdetailsfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView outputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        outputText = (TextView)findViewById(R.id.output);
+
+//        NICDetailFinder obj = new NICDetailFinder("200015500220");
+//
+//        outputText.setText(obj.BIRTH_DAY);
     }
 }
